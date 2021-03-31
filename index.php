b318ebe31f2c974177fa0edd78451449196cc67d
krzysztofic <krzysztof.iciek@gmail.com>
2021-04-01 00:33:19 +0200

    Added wordpress classes and additional php code to plug in css and js files. For now admin panel recognize template as a theme

--- index.php
+++ index.php
@@ -1,4 +1,342 @@
-<?php
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Copywriting Z Wyrazem!</title>
+  <meta name="description" content="Copywriting">
+  <meta name="author" content="">
+  <link rel="shortcut icon" href="<?php echo get_bloginfo('template_directory') ?>/img/icon3.ico" type="image/x-icon">
+
+  <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_directory') ?>/css/bootstrap.css" />
+  <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_directory') ?>/fonts/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_directory') ?>/css/style_main.css" />
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700,800,900" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-GKCR30RGR7"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-GKCR30RGR7');
+  </script>
+
+  <script src="https://www.google.com/recaptcha/api.js?render=6LdClpMaAAAAAHIH9VSulW4SvMSJLLpqWVPGHH44"></script>
+
+  <?php wp_head() ?>
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+  <nav id="menu" class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+          data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span
+            class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+        <!--Logo designed by http://tb-design.pl/ -->
+        <a class="navbar-brand page-scroll" href="#page-top"><img src="<?php echo get_bloginfo('template_directory') ?>/img/logo.png" class="img-responsive" alt=""></a>
+      </div>
+
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#features" class="page-scroll">Oferta</a></li>
+          <li><a href="#about" class="page-scroll">O mnie</a></li>
+          <li><a href="#portfolio" class="page-scroll">Portfolio</a></li>
+          <li><a href="#contact" class="page-scroll">Kontakt</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header id="header">
+    <div class="intro">
+      <div class="overlay">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8 col-md-offset-2 intro-text">
+              <h1>Copywriting z wyrazem<span></span></h1>
+              <p>Teksty z wyrazem na Twoją stronę internetową... i nie tylko</p>
+              <a href="#features" class="btn btn-custom btn-lg page-scroll">Więcej</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <div id="features" class="text-center">
+    <div class="container">
+      <div class="col-md-12 section-title">
+        <h2>Oferta</h2>
+
+        <div class="row">
+          <div class="col-sm-6 col-md-3"> <i class="fa fa-shopping-cart"></i>
+            <h3>Opisy produktów i kategorii</h3>
+          </div>
+          <div class="col-sm-6 col-md-3"> <i class="fa fa-file"></i>
+            <h3>Artykuły na blogi</h3>
+          </div>
+          <div class="col-sm-6 col-md-3"> <i class="fa fa-check-circle"></i>
+            <h3>Korekta tekstów</h3>
+          </div>
+          <div class="col-sm-6 col-md-3"> <i class="fa fa-magic"></i>
+            <h3>Teskty reklamowe</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-md-6"> <img src="<?php echo get_bloginfo('template_directory') ?>/img/about.jpg" class="img-responsive" alt=""> </div>
+        <div class="col-xs-12 col-md-6">
+          <div class="about-text">
+            <h2>Jak pracuję?</h2>
+            <p style="text-align: justify">Czy chcesz mieć treści dostosowane do charakteru Twojej działalności, które
+              zadbają o SEO Twojej witryny,
+              a jednocześnie zainteresują odbiorców? Zamów tekst u mnie i przekonaj się jak wiele można dokonać słowem!
+              Podczas tworzenia planu każdego tekstu mogę zaproponować różne style narracji, dzięki czemu wybierzesz
+              ten, który najbardziej pasuje do charakteru Twojej firmy.
+              Masz dość powtarzających się frazesów w opisach kategorii, produktów oraz treści na Twojej stronie?
+              Stworzę unikalne treści po to, aby Cię wyróżnić! Poznaj copywriting, który nie polega na maszynowym
+              tworzeniu tekstów pod SEO. Dbam o wrażenia odbiorców, SEO, ale też dostosowanie treści do oczekiwań rynku.
+              Nieschematyczne podejście do pisania sprawia, że treści moich klientów realnie wpływają na czytających i
+              zachęcają do zapoznawania się z działaniem firmy oraz zobaczenia oferty.
+              Poznaj moje możliwości! Zamów wycenę, zależy ona od rodzaju tekstu, poziomu eksperckości i kilku innych
+              kwestii. Skorzystaj z formularza i złóż zapytanie o ofertę.</p>
+            <h3>Dlaczego warto ze mną współpracować?</h3>
+            <div class="list-style">
+              <div class="col-lg-6 col-sm-6 col-xs-12">
+                <ul>
+                  <li>Profesjonalne teksty</li>
+                  <li>Zgodność z zasadami SEO</li>
+                  <li>Unikalność treści</li>
+                  <li>Krótkie terminy realizacji</li>
+                </ul>
+              </div>
+              <div class="col-lg-6 col-sm-6 col-xs-12">
+                <ul>
+                  <li>Dokładny research</li>
+                  <li>Poprawność merytoryczna</li>
+                  <li>Ty wybierasz styl!</li>
+                  <li>Indywidualne podejście</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="portfolio" class="text-center">
+    <div class="container">
+      <div class="section-title">
+        <h2>Portfolio</h2>
+        <p>Moje teksty dostępne w sieci (w przygotowniu)</p>
+      </div>
+      <div class="row">
+        <div class="portfolio-items">
 
 
-?>
+          <div class="portfolio-item col-sm-6 col-md-4 col-lg-4">
+            <p>Jak pozycjonują się najlepsi? Wybitni specjaliści SEO sprawdzili techniki polskich liderów e-commerce!
+            </p>
+            <div class="hover-bg"> <a
+                href="https://marketerplus.pl/teksty/artykuly/jak-pozycjonuja-sie-najlepsi-wybitni-specjalisci-seo-sprawdzili-techniki-polskich-liderow-e-commerce/"
+                title="Article in marketer+">
+                <div class="hover-text">
+                  <h4>Markter+</h4>
+                  <article>
+                    Jak pozycjonują się najlepsi specjaliści w branży e-commerce.
+                    Artykuł stworzony dla firmy Semcore.
+                  </article>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="portfolio-item col-sm-6 col-md-4 col-lg-4">
+            <p>Słownik SEO/SEM/SXO (435 haseł)!
+            </p>
+            <div class="hover-bg"> <a href="https://semcore.pl/slownik-seo-sem-sxo/" title="Słownik SEO">
+                <div class="hover-text">
+                  <h4>SEMCORE</h4>
+                  <article>
+                    Szukasz pojęć z zakresu SEO SEM SXO i marketingu internetowego? Nasz słownik SEO to zbiór 435 haseł,
+                    dzięki którym możesz poszerzać swoją wiedzę z tej dziedziny, a także powtarzać sobie najważniejsze
+                    terminy.
+                  </article>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="portfolio-item col-sm-6 col-md-4 col-lg-4">
+            <p>30 wystaw w 30 dni w Poznaniu. </p>
+            <div class="hover-bg"> <a href="https://gloswielkopolski.pl/30-wystaw-w-30-dni-w-poznaniu/ar/3767271"
+                title="Article in Glos Wielkopolski">
+                <div class="hover-text">
+                  <h4>Głos Wielkopolski</h4>
+                  <article>
+                    Już dziś rozpoczyna się projekt stworzony przez studentów oraz kadrę Pracowni Zjawisk Teatralnych
+                    Katedry Scenografii UAP. Przez 30 dni będzie można uczestniczyć w wernisażach, które mają charakter
+                    jednodniowych wystaw. W projekcie przewidziano również spotkanie z twórcami tych wystaw.
+                  </article>
+                </div>
+              </a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="contact">
+    <div class="container">
+      <div class="col-md-8">
+        <div class="row">
+          <div class="section-title">
+            <h2>Skontaktuj się ze mną!</h2>
+            <p style="text-align: justify">Jesteś zainteresowany współpracą? Zapraszam do kontaktu przez poniższy
+              formularz lub telefonicznie.<br>
+              Jeśli chcesz uzyskać wycenę dla swojego zlecenia, wybierz dodatkowo rodzaj zlecenia, liczbę znaków ze
+              spacjami oraz termin realizacji.<br>
+              Informację zwrotną uzyskasz w ciągu 24 godzin.
+            </p>
+          </div>
+          <form name="sentMessage" id="contactForm" novalidate>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response">
+                  <input type="text" id="name" name="name" class="form-control" placeholder="Twoje Imię*"
+                    required="required">
+                  <p class="help-block text-danger"></p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <input type="email" id="email" name="email" class="form-control" placeholder="Email*"
+                    required="required">
+                  <p class="help-block text-danger"></p>
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <textarea name="message" id="message" class="form-control" rows="4"
+                placeholder="Napisz w czym mogę Ci pomóc*" required="required"></textarea>
+              <p class="help-block text-danger"></p>
+            </div>
+            <div class="form-group">
+              <select class="form-control" id="option" name="option">
+                <option value="" disabled selected>Rodzaj tekstu</option>
+                <option value="products">Opisy produktów</option>
+                <option value="blog">Wpisy blogowe</option>
+                <option value="correct">Korekta tekstów</option>
+                <option value="professional">Teksty specjalistyczne</option>
+                <option value="other">Inne zlecenie</option>
+              </select>
+              <p class="help-block text-danger"></p>
+            </div>
+            <div class="form-group">
+              <input type="number" id="char" name="char" class="form-control" placeholder="Liczba znaków ze spacjami"
+                min="500" step="100">
+              <p class="help-block text-danger"></p>
+            </div>
+            <div class="form-group">
+              <input type="text" id="date" name="date" class="form-control" placeholder="Data realizacji"
+                onfocus="(this.type='date')" onblur="(this.type='text')">
+              <p class="help-block text-danger"></p>
+            </div>
+            <div class="form-group">
+              <input type="checkbox" required="required">Wyrażam zgodę na przetwarzanie moich danych osobowych przez Z
+              Wyrazem Paulina Nijak, w celu uzgodnienia szczegółów oferty.*
+              <p class="help-block text-danger"></p>
+              <br>
+              *pola wymagane
+            </div>
+            <div id="success"></div>
+            <button type="submit" class="btn btn-custom btn-lg">Wyślij wiadomość</button>
+          </form>
+
+          <script>
+            grecaptcha.ready(function () {
+              grecaptcha.execute('6LdClpMaAAAAAHIH9VSulW4SvMSJLLpqWVPGHH44', {
+                  action: 'homepage'
+                })
+                .then(function (token) {
+                  document.getElementById('g-recaptcha-response').value = token;
+                });
+            });
+          </script>
+
+        </div>
+      </div>
+      <div class="col-md-3 col-md-offset-1 contact-info">
+        <div class="contact-item">
+          <h3>Informacje kontaktowe</h3>
+          <p><span><i class="fa fa-map-marker"></i> Adres</span>Z Wyrazem Paulina Nijak<br>
+            ul. Słowackiego 55/1<br>
+            60-521 Poznań<br>
+            NIP: 7792516733</p>
+        </div>
+        <div class="contact-item">
+          <p><span><i class="fa fa-phone"></i> Telefon</span> +48 660 841 637</p>
+        </div>
+        <div class="contact-item">
+          <p><span><i class="fa fa-envelope-o"></i> Email</span> kontakt@zwyrazem.pl</p>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <div class="row">
+          <div class="social">
+            <ul>
+              <li><a href="https://www.linkedin.com/in/paulina-nijak-930352170/"><i class="fa fa-linkedin"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="footer">
+    <div class="container text-center">
+      <p>Z Wyrazem 2020 Build by <a href="https://github.com/krzysztofic" rel="nofollow">Krzysztofic</a></p>
+    </div>
+  </div>
+  <script type="text/javascript" src="<?php echo get_bloginfo('template_directory') ?>/js/jquery.1.11.1.js"></script>
+  <script type="text/javascript" src="<?php echo get_bloginfo('template_directory') ?>/js/bootstrap.js"></script>
+  <script type="text/javascript" src="<?php echo get_bloginfo('template_directory') ?>/js/SmoothScroll.js"></script>
+  <script type="text/javascript" src="<?php echo get_bloginfo('template_directory') ?>/js/nivo-lightbox.js"></script>
+  <script type="text/javascript" src="<?php echo get_bloginfo('template_directory') ?>/js/jqBootstrapValidation.js"></script>
+  <script type="text/javascript" src="<?php echo get_bloginfo('template_directory') ?>/js/jquery.counterup.js"></script>
+  <script type="text/javascript" src="<?php echo get_bloginfo('template_directory') ?>/js/waypoints.js"></script>
+  <script type="text/javascript" src="<?php echo get_bloginfo('template_directory') ?>/js/contact_me.js"></script>
+  <script type="text/javascript" src="<?php echo get_bloginfo('template_directory') ?>/js/main.js"></script>
+
+    <?php wp_footer() ?>
+</body>
+
+</html>
